d8f79f37d40cce329f51bf306219bac40e2d8c4c
ashutosh <ashutoshmathur1711@gmail.com>
2025-12-08 00:08:35 +0530

    rm .env changed db to postgres

--- backend/settings.py
+++ backend/settings.py
@@ -71,8 +71,6 @@ GOOGLE_OAUTH_SCOPES = ["https://www.googleapis.com/auth/gmail.send"]
 
 FRONTEND_URL= os.getenv("FRONTEND_URL")
 
-print(">>>>>>>>...1",GOOGLE_OAUTH_CLIENT_ID)
-print(">>>>>>>>>>>>>>>2",GOOGLE_OAUTH_CLIENT_SECRET)
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -100,10 +98,15 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("DB_NAME"),
+        "USER" : os.getenv("DB_USER"),
+        "PASSWORD" : os.getenv("DB_PASSWORD"),
+        "HOST" : os.getenv("DB_HOST"),
+        "PORT" : os.getenv("DB_PORT")
     }
 }
 
